9b1c317ead19080d1c0ee276c4787bfe3face3be
Harshit Arora <94703188+harshitarora00@users.noreply.github.com>
2024-07-30 18:08:24 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ import Navbar from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "IMDB",
+  title: "Movie Mania",
   description: "By Harshit Arora",
 };
 
