a87d8e7fa40e6f72420239f1e79ff08c6d1a9441
Harshit Arora <94703188+harshitarora00@users.noreply.github.com>
2024-07-30 22:43:32 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   output: {
+     "export"
+   },  // <=== enables static exports
+  reactStrictMode: { 
+    true
+  },
   images: {
     domains: ["image.tmdb.org"],
   },
